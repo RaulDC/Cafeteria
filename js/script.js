19402635c6eed681d0b7d4492016a8e833c5b165
RaulDC <81639203+RaulDC@users.noreply.github.com>
2023-09-04 18:41:21 -0500

    Función de alerta del botón de reserva

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Obtener el botón de reserva
+const botonReservar = document.getElementById('reservar-btn');
+
+// Agregar un evento click al botón
+botonReservar.addEventListener('click', () => {
+    alert('¡Mesa reservada con éxito!');
+});
