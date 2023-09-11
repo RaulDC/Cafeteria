cb8945a77bfdc4479f0a523672a9a86e5b83f325
RaulDC <81639203+RaulDC@users.noreply.github.com>
2023-09-10 21:35:23 -0500

    Actualizar script

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ const botonReservar = document.getElementById('reservar-btn');
 
 // Agregar un evento click al botón
 botonReservar.addEventListener('click', () => {
-    alert('¡Mesa reservada con éxito!');
+    alert('¡Verifica tu correo electrónico para realizar tu reserva! Tu código de descuento del 20% es: nutriacostada99');
 });
